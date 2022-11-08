f7f5b6848874c740f91e8a83bd5e1c4d5e706c4a
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-09 05:48:40 +0800

    ci:added colors vriables

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        primary:'#fb5c7e',
+        primary:'#f856a6', 
         secondary:"#6b81f9",
         dark:"#343c4f",
         accent:"#4cc2ef",
