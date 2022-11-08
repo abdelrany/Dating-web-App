98bb41d29214cf734f65202ff7387f31c9453060
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-09 06:04:15 +0800

    feat: removed the code of home page to sperated components

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
+import UnloggedHome from '../components/home/UnloggedHome'
 import "../styles/globals.css"
 
 type Props = {}
-
 const HomePage = (props: Props) => {
   return (
-    <div className='min-h-screen my-0 mx-auto max-w-[1200px] pt-24'>
-      {/* each page will a different nav besides the main nav */}
-      <div className="absolute top-16 left-0 w-screen text-dark bg-gradient">
-        <ul className='flex items-center gap-16  max-w-[1200px] my-0 mx-auto h-16'>
-            <li className="">My Matches</li>
-            <li className="">Favorites</li>
-            <li className="">Visitors</li>
-            <li className="">Likes</li>
-            <li className="">Hidden Matches</li>
-        </ul>
-      </div>
+    <div className='min-h-screen'>
 
-        Home page
+        <UnloggedHome />
     </div>  
   )
 }
