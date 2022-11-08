d7445608b2d4f703d95bb2eaaa8d24cf739b17b0
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-09 06:00:38 +0800

    feat: added home screen when user is not logged in

---
+++ front-end/components/home/UnloggedHome.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import React from 'react'
+import Form from './Form'
+import homeImg from '../../assets/images/home.jpeg'
+import privacyImg from '../../assets/images/privacy.png'
+import secureImg from '../../assets/images/securee.png'
+import {ChatBubbleOvalLeftIcon, HeartIcon, UserPlusIcon } from '@heroicons/react/24/outline'
+import Features from './Features'
+
+type Props = {}
+
+const UnloggedHome = (props: Props) => {
+  return (
+    <section className=''>
+       <Image src={homeImg} alt="dating app home page" className="absolute top-0 left-0 w-screen h-screen z-[-1]" />
+       <div className='py-8 md:px-12 lg:px-32 mb-12 h-[75vh]'>
+           <h1 className='font-extrabold text-4xl text-center text-gray-100 w-[350px] mb-4'>Find Real Love in Real Time at a Click</h1>
+           <Form/>
+           <div className='absolute bottom-44 right-32 flex items-center rounded-md bg-gray-100 w-90 h-16'>
+               <Image src={privacyImg} alt="privacy" className='h-full w-32' />
+               <Image src={secureImg} alt="privacy" className='h-full w-44' />
+           </div>
+       </div>
+        <div className='h-28 bg-gray-100 flex items-center justify-around md:px-12 lg:px-32 '>
+                <div className='flex flex-col justify-center items-center translate-y-[-40px] gap-4'>
+                    <div className='flex justify-center items-center w-12 h-12 rounded-full bg-primary group hover:bg-gray-100 duration-300'>
+                        <UserPlusIcon className='h-6 w-6 text-gray-100 group-hover:text-primary'/> 
+                    </div>
+                    <h1 className='text-center font-extrabold text-dark'>Sign Up and Get Started</h1>
+                </div>
+                <div className='flex flex-col justify-center items-center translate-y-[-40px] gap-4'>
+                    <div className='flex justify-center items-center w-12 h-12 rounded-full bg-primary group hover:bg-gray-100 duration-300'>
+                        <ChatBubbleOvalLeftIcon className='h-6 w-6 text-gray-100 group-hover:text-primary'/> 
+                    </div>
+                    <h1 className='text-center font-extrabold text-dark'>Unlock "Ready To Chat"</h1>
+                </div>
+                <div className='flex flex-col justify-center items-center translate-y-[-40px] gap-4'>
+                    <div className='group flex justify-center items-center w-12 h-12 rounded-full bg-primary hover:bg-gray-100 duration-300'>
+                        <HeartIcon className='h-6 w-6 text-gray-100 group-hover:text-primary duration-300'/> 
+                    </div>
+                    <h1 className='text-center font-extrabold text-dark'>Invite your match to date</h1>
+                </div>
+        </div>
+        <Features/>
+    </section>
+  )
+}
+export default UnloggedHome
