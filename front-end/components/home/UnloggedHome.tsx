bd4698dc36c716a597b2c71e61bbf47bf79e17d4
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-10 01:51:32 +0800

    imported other sections

--- front-end/components/home/UnloggedHome.tsx
+++ front-end/components/home/UnloggedHome.tsx
@@ -4,8 +4,12 @@ import Form from './Form'
 import homeImg from '../../assets/images/home.jpeg'
 import privacyImg from '../../assets/images/privacy.png'
 import secureImg from '../../assets/images/securee.png'
+import datenewsLogo from '../../assets/images/datingnews.png'
+import dateadviceLogo from '../../assets/images/datingadvice.png'
+
 import {ChatBubbleOvalLeftIcon, HeartIcon, UserPlusIcon } from '@heroicons/react/24/outline'
 import Features from './Features'
+import DateAdviceSection from './DateAdviceSection'
 
 type Props = {}
 
@@ -22,19 +26,19 @@ const UnloggedHome = (props: Props) => {
            </div>
        </div>
         <div className='h-28 bg-white flex items-center justify-around gap-2 px-2 sm:px-8 lg:px-32 '>
-                <div className='flex flex-col justify-center items-center -translate-y-12 gap-4'>
+                <div className='flex flex-col justify-center items-center -translate-y-10 gap-4'>
                     <div className='flex justify-center items-center w-12 h-12 rounded-full bg-primary group hover:bg-gray-100 duration-300'>
                         <UserPlusIcon className='h-6 w-6 text-gray-100 group-hover:text-primary'/> 
                     </div>
                     <h1 className='text-center text-sm lg:font-extrabold text-dark'>Sign Up and Get Started</h1>
                 </div>
-                <div className='flex flex-col justify-center items-center  -translate-y-12 gap-4'>
+                <div className='flex flex-col justify-center items-center  -translate-y-10 gap-4'>
                     <div className='flex justify-center items-center w-12 h-12 rounded-full bg-primary group hover:bg-gray-100 duration-300'>
                         <ChatBubbleOvalLeftIcon className='h-6 w-6 text-gray-100 group-hover:text-primary'/> 
                     </div>
                     <h1 className='text-center text-sm lg:font-extrabold text-dark'>Unlock "Ready To Chat"</h1>
                 </div>
-                <div className='flex flex-col justify-center items-center  -translate-y-12 gap-4'>
+                <div className='flex flex-col justify-center items-center  -translate-y-10 gap-4'>
                     <div className='group flex justify-center items-center w-12 h-12 rounded-full bg-primary hover:bg-gray-100 duration-300'>
                         <HeartIcon className='h-6 w-6 text-gray-100 group-hover:text-primary duration-300'/> 
                     </div>
@@ -42,6 +46,14 @@ const UnloggedHome = (props: Props) => {
                 </div>
         </div>
         <Features/>
+        <DateAdviceSection />
+        <div className='bg-white py-12 px-2 sm:px-8 lg:px-32 '>
+            <h1 className='text-dark text-3xl font-extrabold text-center mb-12'>Featured on </h1>
+            <div className='flex justify-center gap-24 items-center'>
+                <Image src={datenewsLogo} alt="Dating news" className='w-24 h-16 md:w-32 md:h-24'/>
+                <Image src={dateadviceLogo} alt="dating advice" className='w-24 h-16 md:w-32 md:h-24'/>
+            </div>
+        </div>
     </section>
   )
 }
