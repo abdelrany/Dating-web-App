587480aa835f1b50d32cdba50fe3ef0bd21a8d0f
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-09 22:03:38 +0800

    feat: added new section in the home page

---
+++ front-end/components/home/DateAdviceSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+const DateAdviceSection = (props: Props) => {
+  return (
+    <section className=''>
+        <article className=''>
+            <h1 className=''>Datign Advice For Singles</h1>
+            <p className=''>
+                Looking for love ? There is a dizzying amount of advice out there. You can overcome your obstacles.
+                
+            </p>
+        </article>
+    </section>
+  )
+}
+
+export default DateAdviceSection
