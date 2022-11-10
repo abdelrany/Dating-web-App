5e9fee9291ee0c23cec614f0515b1798e0f733f7
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-10 23:26:28 +0800

    feat: added padding

--- front-end/components/home/LoggedinHome.tsx
+++ front-end/components/home/LoggedinHome.tsx
@@ -5,14 +5,15 @@ type Props = {}
 const LoggedinHome = (props: Props) => {
   return (
     <section className=''>
-        <div className="absolute top-12 left-0 w-screen text-dark bg-gradient">
-            <ul className='flex items-center gap-16 text-gray-200  px-2 sm:px-8 h-12'>
+        <div className="absolute top-12 left-0 w-screen text-dark bg-gradient  px-2 sm:px-8 lg:px-32">
+            <ul className='flex items-center gap-16 text-gray-200 h-12'>
                 <li className="">My Matches</li>
                 <li className="">Favorites</li>
                 <li className="">Visitors</li>
                 <li className="">Likes</li>
                 <li className="">Hidden Matches</li>
             </ul>
+            
        </div>
 
     </section>
