df1c64c06d0cfec343ced88c648cdd203cc9fff2
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-11 07:27:53 +0800

    feat: added global fonts

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,21 +1,26 @@
 import Footer from "../components/Footer"
 import Nav from "../components/Nav"
 import "../styles/globals.css"
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+import { Inter } from '@next/font/google';
+
+const inter = Inter();
+
+
+export default function RootLayout({ children}: { children: React.ReactNode}) {
+
   return (
     <html className="">
-      <head>
-        <title>DatingApp | Find Your Dream Partner Online</title>
+      <head className={inter.className}>
+          <title>ClickDate | Find Your Dream Partner Online</title>
       </head>
+
       <body className="scroll-smooth">
         <Nav/>
         {children}
         <Footer/>
       </body>
+
     </html>
   )
 }
