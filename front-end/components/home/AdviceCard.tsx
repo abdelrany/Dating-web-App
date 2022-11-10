6d901777f4f90728666ec9a2c1660061c29e6ddb
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-10 23:25:38 +0800

    style:removed spaces

--- front-end/components/home/AdviceCard.tsx
+++ front-end/components/home/AdviceCard.tsx
@@ -12,7 +12,9 @@ type Props = {
 const AdviceCard = ({image, title, date, description}: Props) => {
   return (
     <div className='group rounded-md overflow-hidden shadow hover:shadow-lg duration-300 cursor-pointer bg-white '>
-        <Image src={exampleImg} alt="a dating advice"  className="group-hover:scale-105 w-full h-44 duration-300"/>
+        <div className='w-full h-56 overflow-hidden'>
+          <Image src={exampleImg} alt="a dating advice"  className="group-hover:scale-105 w-full h-full duration-300"/>
+        </div>
         <div className='p-4'>
             <p className='text-gray-600/50 mb-2 text-sm'>{date}</p>
             <h1 className='font-extrabold text-xl mb-2 text-dark'>{title}</h1>
