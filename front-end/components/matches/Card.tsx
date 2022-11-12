c829b900516567e06b13a5414a1d5408ea5174dc
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-13 01:57:34 +0800

    feat: added hover effect to icons

--- front-end/components/matches/Card.tsx
+++ front-end/components/matches/Card.tsx
@@ -10,10 +10,10 @@ const Card = (props: Props) => {
         <article className='group relative h-[340px] rounded-md overflow-hidden shadow-md flex flex-col bg-white'>
             <Image src={profileImg} className='w-full flex-[.9]' alt="clickdate"/>
             <div className='flex-[.1] px-2 py-4 flex justify-between items-center'>
-                <EnvelopeIcon className='w-6 h-6 text-primary cursor-pointer'/>
-                <FireIcon className='w-6 h-6 text-primary cursor-pointer'/>
-                <EyeIcon className='w-6 h-6 text-primary cursor-pointer'/>
-                <StarIcon className='w-6 h-6 text-primary cursor-pointer z-10'/>
+                <EnvelopeIcon className='w-6 h-6 text-primary hover:text-secondary cursor-pointer'/>
+                <FireIcon className='w-6 h-6 hover:text-secondary text-primary cursor-pointer'/>
+                <EyeIcon className='w-6 h-6 hover:text-secondary text-primary cursor-pointer'/>
+                <StarIcon className='w-6 h-6 hover:text-secondary text-primary cursor-pointer z-10'/>
             </div>
             <div className=' bg-white absolute bottom-0 left-0 group-hover:bottom-12 h-12 group-hover:h-[180px] w-full transition-all duration-400 ease-out rounded p-2 flex flex-col gap-2'>
                 <h1 className='font-extrabold text-lg text-dark tracking-wide'>Alxandro Herakez</h1>
