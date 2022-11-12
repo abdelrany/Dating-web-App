dc9ba78ff97cc669e0baa8679e986689491ed3f4
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-13 02:56:21 +0800

    feat: removed the sharednav inks

--- front-end/app/discovery/page.tsx
+++ front-end/app/discovery/page.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import SharedPageNav from '../../components/SharedPageNav'
 
-import { ArchiveBoxIcon, BellAlertIcon, InboxArrowDownIcon } from '@heroicons/react/24/outline'
 
-const sharednavLinks = [
-  {title:'Inbox', icon: <InboxArrowDownIcon  className='h-5 w-5'/>},
-  {title:'Archive', icon: <ArchiveBoxIcon className='h-5 w-5'/>},
-  {title:'Notifications', icon: <BellAlertIcon className='h-5 w-5'/>},
-]
 
 const discovery = () => {
   return (
     <div className='min-h-screen shadow'>
-        <SharedPageNav sharednavLinks={sharednavLinks}/>
+        dicovery
     </div>
   )
 }
