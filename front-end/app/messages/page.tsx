9e96151d86e90b0937969da2847ecd0f576463db
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-12 21:13:41 +0800

    feat: added responsiveness and more fonctuanalities

--- front-end/app/messages/page.tsx
+++ front-end/app/messages/page.tsx
@@ -1,32 +1,40 @@
-import { ArchiveBoxIcon, BellAlertIcon, InboxArrowDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-import React from 'react'
+"use client"
+import { ArchiveBoxIcon, ArrowUturnLeftIcon, BellAlertIcon, InboxArrowDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { NextPage } from 'next'
+import React, { FC, useState } from 'react'
 import BoxHeader from '../../components/messages/BoxHeader'
 import ContactItem from '../../components/messages/ContactItem'
 import MsgInputBox from '../../components/messages/MsgInputBox'
 import SharedPageNav from '../../components/SharedPageNav'
 
 type Props = {}
-
+type TchatBox = {
+  id:number
+  name:string
+}
 const sharednavLinks = [
   {title:'Inbox', icon: <InboxArrowDownIcon  className='h-5 w-5'/>},
   {title:'Archive', icon: <ArchiveBoxIcon className='h-5 w-5'/>},
   {title:'Notifications', icon: <BellAlertIcon className='h-5 w-5'/>},
 ]
 
-const messages = (props: Props) => {
+const Messages:NextPage = (props: Props) => {
+  // this is very helpful for phone version, so that we open chatbox only when we click a recent chat 
+  const [ChatBox, setChatBox] = useState<TchatBox | null>(null) //
+
   return (
     <section className='px-2 lg:px-32 pt-16 h-screen w-full bg-gray-200'>
       <SharedPageNav sharednavLinks={sharednavLinks}/>
 
-      <div className='w-full h-[90vh] bg-white rounded-lg overflow-hidden border shadow md:grid md:grid-cols-3'>
-             <div className='col-span-1 relative pt-20 overflow-hidden h-full border-r-2'>
+      <div className='w-full h-[90vh] relative bg-white rounded-lg overflow-hidden border shadow md:grid md:grid-cols-3'>
+             <div className={`${ChatBox && "hidden md:block"} col-span-1 relative pt-20 overflow-hidden h-full border-r-2`}>
                  <div className=' flex items-center pl-6 pr-2 absolute bg-gray-100 top-0 right-0 left-0 h-20 border-b-2'>
                     <MagnifyingGlassIcon className='w-6 h-6 -mr-8 z-10 text-primary'/>    
                     <input type="text"  placeholder='Type names for quick find'
                           className='bg-white w-full h-10 rounded-lg pl-10'
                     />
                  </div>
-                 <div className='overflow-scroll h-full'>
+                 <div className='overflow-scroll h-full' onClick={()=>setChatBox({id:2, name:"Chris Molaga"})}>
                    {/* here we should render the list of recent chats */}
                     <ContactItem/>
                     <ContactItem/>
@@ -38,19 +46,27 @@ const messages = (props: Props) => {
                     <ContactItem/>
                     <ContactItem/>
 
-                    {/* When there are no messages yet means has never talked to someone, we render this message  */}
+                    {/* we render this message, When there are no messages yet means has never talked to someone */}
                     {/* <p className='text-primary text-xl ml-24 mt-72'>No Chats yet !</p> */}
                  </div>
             </div>
-            <div className='h-full overflow-hidden shadow-md md:col-span-2 flex flex-col items-stretch justify-between'>
-                  <BoxHeader />
-                  <div className='w-full flex-1 overflow-scroll'>
-                          {/* messages */}
-                  </div>
+            {/* goBack icon is shown ony in mobile version and when there is a chat selected */}
+            <ArrowUturnLeftIcon  onClick={()=> setChatBox(null)}
+                  className={`${!ChatBox && "hidden"} cursor-pointer w-6 h-6 text-primary absolute md:hidden top-6 left-2`}
+              />
+              {/* this is where the chats will happen, and it display only whe a chat is selected */}
+            <div className={` ${ChatBox ? "flex " : "hidden"}  h-full overflow-hidden shadow-md md:col-span-2  flex-col items-stretch justify-between`}>
+                  
+                 {ChatBox &&  <BoxHeader id={ChatBox!.id || 0}  name={ChatBox!.name || "no user"} /> }
+
                   <MsgInputBox/>
             </div>
+            {/* we render this message when there is not chat selected */}
+            <h1 className={`${ChatBox && "hidden"} text-primary text-2xl font-bold mt-72 ml-52`}>
+                  Please select a person to talk to
+            </h1>
       </div>
     </section>
   )
 }
-export default messages
+export default Messages
--- front-end/components/messages/BoxHeader.tsx
+++ front-end/components/messages/BoxHeader.tsx
@@ -3,22 +3,30 @@ import React from 'react'
 import imgProfile from '../../assets/images/profile.png'
 import { ArchiveBoxIcon, EnvelopeIcon, FlagIcon,  } from '@heroicons/react/24/outline'
 
-type Props = {}
+type Props = {
+  id:number
+  name:string
+}
 
 const BoxHeader = (props: Props) => {
   return (
-   <div className='h-20 bg-gray-100 border-b-2 w-full flex items-center justify-between px-4 '>
-       <div className='flex items-center gap-4'>
-          <Image src={imgProfile} alt="clickdate user" className='rounded-full w-12 h-12'/>
-          <h1 className='font-extrabold text-md tracking-wide'>Xavier Hernandez</h1>
-       </div>
-
-       <div className='flex gap-4 items-center'>
-            <EnvelopeIcon className='cursor-pointer h-5 w-5 text-dark duration-300'/>
-            <FlagIcon className='cursor-pointer h-5 w-5 text-dark duration-300'/>
-            <ArchiveBoxIcon className='cursor-pointer h-5 w-5 text-dark duration-300'/>
+    <>
+    <div className='h-20 bg-gray-100 border-b-2 w-full flex items-center justify-between px-4 '>
+        <div className='flex items-center gap-4 ml-8 md:ml-0'>
+            <Image src={imgProfile} alt="clickdate user" className='rounded-full w-12 h-12'/>
+            <h1 className='font-extrabold text-md tracking-wide'>{props.name}</h1>
         </div>
-    </div>
+
+        <div className='flex gap-4 items-center'>
+              <EnvelopeIcon className='cursor-pointer h-5 w-5 text-dark duration-300'/>
+              <FlagIcon className='cursor-pointer h-5 w-5 text-dark duration-300'/>
+              <ArchiveBoxIcon className='cursor-pointer h-5 w-5 text-dark duration-300'/>
+          </div>
+      </div>
+      <div className='w-full flex-1 overflow-scroll'>
+                        {/* messages */}
+      </div>
+  </>
   )
 }
 
