b82aa40554f8dd270c7e367ed364aa30affafbb8
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-13 00:05:57 +0800

    feat: added matches and a card component

--- front-end/app/matches/page.tsx
+++ front-end/app/matches/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SharedPageNav from '../../components/SharedPageNav'
 import { UserGroupIcon, StarIcon, HandThumbUpIcon, EyeSlashIcon, HeartIcon } from '@heroicons/react/24/outline'
+import Card from '../../components/matches/Card'
 
 const sharednavLinks = [
   {title:'My matches', icon: <HeartIcon className='h-5 w-5'/>},
@@ -12,8 +13,23 @@ const sharednavLinks = [
 
 const Matches = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='px-2 sm:px-8 lg:px-32 pt-12 min-h-screen bg-gray-100 flex flex-col items-center'>
       <SharedPageNav sharednavLinks={sharednavLinks}/>
+      <main className='relative grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4 py-8'>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+         <Card/>
+      </main>
+      <button className='mx-auto bg-gray-300 text-secondary tracking-wide my-4 px-4 py-2 rounded-md cursor-pointer'>Load More</button>
     </div>
   )
 }
---
+++ front-end/components/matches/Card.tsx
@@ -0,0 +1,28 @@
+import { EnvelopeIcon, EyeIcon, FireIcon, StarIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import React from 'react'
+import profileImg from '../../assets/images/profile.png'
+
+type Props = {}
+
+const Card = (props: Props) => {
+  return (
+        <article className='group relative h-[340px] rounded-md overflow-hidden shadow-md flex flex-col bg-white'>
+            <Image src={profileImg} className='w-full flex-[.9]' alt="clickdate"/>
+            <div className='flex-[.1] px-2 py-4 flex justify-between items-center'>
+                <EnvelopeIcon className='w-6 h-6 text-primary cursor-pointer'/>
+                <FireIcon className='w-6 h-6 text-primary cursor-pointer'/>
+                <EyeIcon className='w-6 h-6 text-primary cursor-pointer'/>
+                <StarIcon className='w-6 h-6 text-primary cursor-pointer z-10'/>
+            </div>
+            <div className=' bg-white absolute bottom-0 left-0 group-hover:bottom-12 h-12 group-hover:h-[180px] w-full transition-all duration-400 ease-out rounded p-2 flex flex-col gap-2'>
+                <h1 className='font-extrabold text-lg text-dark tracking-wide'>Alxandro Herakez</h1>
+                <p className='text-sm tracking-wide text-primary'>Is Online</p>
+                <p className='text-md text-dark tracking-wide'>Lives in  NY city</p>
+                <p className='text-md text-dark tracking-wide'>Works at a school</p>
+                <p className='text-md text-dark tracking-wide'>Bachelor's degree</p>
+            </div>
+        </article>
+  )
+}
+export default Card
